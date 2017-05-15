606715182008a8a3d90b4c4fb73ff563e21f367b
newnakashima <newnakashima2014@gmail.com>
2017-05-15 20:14:12 +0900

    ヘルパーとABOUTページを作成した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def nl2br(str)
+    return sanitize(str).gsub("\n", "<br>").html_safe
+  end
+
+  def md5_hash(phrase)
+    return Digest::MD5.new.update(phrase.to_s)
+  end
+
+  def login?(user, pass)
+    if user != nil
+      return md5_hash(pass).to_s == user.pass
+    else
+      return false
+    end
+  end
+
+  def loggedIn?
+    result = !session[:login_info].nil? || !defined? session[:login_info]
+    puts result
+    return result
+  end
+
 end

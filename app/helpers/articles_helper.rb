88225aa133427cee1d8eb422591ad3e5fec1f454
newnakashima <newnakashima2014@gmail.com>
2017-07-17 16:43:16 +0900

    簡易で画像を表示

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,4 +3,12 @@ module ArticlesHelper
     return false if page.to_i <= 0 
     return true
   end
+
+  def translateImageTag(text)
+    newText = ""
+    text.each_line("\n") {|line|
+      newText += line.gsub(/\[img:\s(.*)\]/, '<img \1>')
+    }
+    return newText
+  end
 end
